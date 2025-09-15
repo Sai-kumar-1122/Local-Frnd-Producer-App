a15a8abdc3588552f01c1706922b24de9acb9d10
vijaya-durga-12 <arepallivijayadurga12@gmail.com>
2025-09-15 17:02:36 +0530

    first commit of producer-app

---
+++ App.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text>LOCAL-FRND-PRODUCER-APP</Text>
+    </View>
+  )
+}
+
+export default App
